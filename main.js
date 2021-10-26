0de420d4607233fe3ced2e9b7d9d0c4d248c225e
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-10-26 18:48:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+img_bedroom="";
+img_apple="";
+img_basket="";
+img_bottle="";
+img_tv="";
+
+function preload(){
+    img_bedroom= loadImage('bedroom.jpg');
+    img_apple=loadImage('all_apple.jpg');
+    img_basket=loadImage('basket.jpg');
+    img_bottle=loadImage('bottle.jpg');
+    img_tv=loadImage('tv.jpg')
+ }
+function page_bedroom(){
+    window.location="page_bedroom.html";
+}
+function page_bottle(){
+    window.location="page_bottle.html";
+}
+function page_apple(){
+    window.location="page_apple.html";
+}
+function page_basket(){
+    window.location="page_basket.html";
+}
+function page_tv(){
+    window.location="page_tv.html";
+}
